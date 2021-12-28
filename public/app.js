12b6fa0aaa41f820609633e5d85ee02ce4d67e92
USamaAbdulJAbbar <usamabest4u@gmail.com>
2021-12-29 00:18:35 +0500

    Work Complete

---
+++ public/app.js
@@ -0,0 +1,10 @@
+// var b  = document.getElementById('getname');
+// var a = document.getElementById('name').innerHTML = b;     
+
+
+var a = document.images;
+
+var b = document.scripts;
+console.log(b);
+console.log(a);
+
